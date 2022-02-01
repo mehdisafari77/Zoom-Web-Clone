cfc838b62862e790d893759af4718911cc9059d0
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-01-31 20:34:28 -0800

    add peer features to access microphone and camera and to view other users camera plus creating a message function

--- public/main.js
+++ public/main.js
@@ -33,3 +33,50 @@ sendmessage = (text) => {
         main__chat_window.scrollTop = main__chat_window.scrollHeight;
     }
 };
+
+// Webrtc provides a standard api for accessing cameras and microphones connected to the device
+navigator.mediaDevices 
+    .getUserMedia({
+        video: true,
+        audio: true,
+    })
+    .then((stream) => {
+        myVideoStream = stream;
+        addVideoStream(myVideo, stream, myname);
+
+        socket.on("user-connected", (id, username) => {
+            connectToNewUser(id, stream, username); 
+            socket.emit("tellName", myname);
+        });
+
+        socket.on("user-disconnected", (id) => {
+            console.log(peers);
+            if (peers[id]) peers[id].close();
+        });
+    });
+peer.on("call", (call) => { 
+    getUserMedia({ video: true, audio: true },
+        function(stream) {
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", function(remoteStream) {
+                addVideoStream(video, remoteStream, OtherUsername);
+            });
+        },
+        function(err) {
+            console.log("Failed to get local stream", err);
+        }
+    );
+});
+
+peer.on("open", (id) => {
+    socket.emit("join-room", roomId, id, myname); 
+});
+
+socket.on("createMessage", (message) => {
+    var ul = document.getElementById("messageadd");
+    var li = document.createElement("li");
+    li.className = "message";
+    li.appendChild(document.createTextNode(message));
+    ul.appendChild(li);
+});
